d2f381472419288bb09fa678e56bb5cb39eedc3b
0xjac <git@0xjac.com>
2024-08-06 01:57:13 +0200

    fix: pointer receiver for levelDB storage

--- store/leveldb.go
+++ store/leveldb.go
@@ -10,20 +10,20 @@ type LevelDB struct {
 	*leveldb.DB
 }
 
-func (a LevelDB) Get(key []byte) ([]byte, error) {
-	return a.DB.Get(key, nil)
+func (l *LevelDB) Get(key []byte) ([]byte, error) {
+	return l.DB.Get(key, nil)
 }
 
-func (a LevelDB) Put(key, value []byte) error {
-	return a.DB.Put(key, value, nil)
+func (l *LevelDB) Put(key, value []byte) error {
+	return l.DB.Put(key, value, nil)
 }
 
-func (a LevelDB) Delete(key []byte) error {
-	return a.DB.Delete(key, nil)
+func (l *LevelDB) Delete(key []byte) error {
+	return l.DB.Delete(key, nil)
 }
 
-func NewLevelDB(dbPath string) (LevelDB, error) {
+func NewLevelDB(dbPath string) (*LevelDB, error) {
 	db, err := leveldb.OpenFile(dbPath, nil)
 
-	return LevelDB{db}, err
+	return &LevelDB{db}, err
 }
